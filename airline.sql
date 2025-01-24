51ac248ccaa2f6184549bd3e4f3d89dc95025fb8
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2025-01-24 11:23:47 +0530

    sql

---
+++ airline.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE airline;
+USE airline;
+
+DROP DATABASE airline;
+ALTER TABLE user DROP COLUMN email_id;
+SHOW INDEXES FROM user;
+SHOW TABLES;
+
+DESC user;
+SELECT * FROM user;
+
+DESC ticket;
+SELECT * FROM ticket;
+
+DESC seat;
+
